32bdbaed0c0f4c3259ceb2e4464ce0493ff25f28
Geovanny Alvarez <geovanny0.0lopez@gmail.com>
2023-03-10 18:33:15 -0600

    ✨ : receive file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { EnvConfiguration } from './config/app.config';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { SeedModule } from './seed/seed.module';
     ProductsModule,
     CommonModule,
     SeedModule,
+    FilesModule,
   ],
 })
 export class AppModule {}
