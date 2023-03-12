610344d72e1dc89cec7ad6418ca883b8a34e2fb6
Geovanny Alvarez <geovanny0.0lopez@gmail.com>
2023-03-12 12:41:13 -0600

    ✨: Recuperar archivos con ruta segura

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,18 +1,36 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileNamerHelper, fileFilterHelper } from './helpers';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -27,6 +45,8 @@ export class FilesController {
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('File is empty');
 
-    return { fileName: file };
+    return `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
@@ -13,6 +14,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`App running on port ${port}`);
 }
 bootstrap();
